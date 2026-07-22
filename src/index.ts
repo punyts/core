4f926ca6c3598c1f99385c0df6bec08c300de50e
Rodger McIntosh <rodger@geekshake.com>
2026-07-21 21:41:24 -0600

    Fixed issue with node:fs in browsers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./logging/Reporter.js";
 export * from "./logging/ConsoleLogger.js";
-export * from "./logging/FileLogger.js";
 export * from "./RuntimeType.js";
 export * from "./validation/index.js";
